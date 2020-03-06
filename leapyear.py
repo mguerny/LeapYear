51f2bee934248eb6d2ee8161568df8d8f32bc63f
mguerny <marin.guerny@laposte.net>
2020-03-06 12:20:32 +0100

    BLUE - test  4

--- leapyear.py
+++ leapyear.py
@@ -6,10 +6,7 @@ class LeapYear:
     @staticmethod
     def leapyear(year):
 
-        if year % 100 == 0 and year % 400 != 0:
-            return False
-
-        if year % 4 != 0:
+        if (year % 100 == 0 and year % 400 != 0) or year % 4 != 0:
             return False
 
         if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
