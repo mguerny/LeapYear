63a311b6175390e8d3ba9d29d66bca85d6c68b4f
mguerny <marin.guerny@laposte.net>
2020-03-06 12:17:28 +0100

    BLUE - test 3

--- leapyear.py
+++ leapyear.py
@@ -1,17 +1,15 @@
 class LeapYear:
 
+    # leap years example : 1600, 2000, 2400, 2004, 2008
+    # NOT leap years example : 1700, 1800, 2017, 2018
+
     @staticmethod
     def leapyear(year):
-        answer = False
-
-        if year % 400 == 0:
-            answer = True
 
         if year % 100 == 0 and year % 400 != 0:
             return False
 
-        if year % 4 == 0 and year % 100 != 0:
-            answer = True
+        if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+            return True
 
-        return answer
-
+        return False
