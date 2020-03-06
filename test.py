ed442d4af932993f360b606845a3c8c85633fbc8
mguerny <marin.guerny@laposte.net>
2020-03-06 12:08:22 +0100

    RED - test 3

--- test.py
+++ test.py
@@ -8,7 +8,10 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(LeapYear.leapyear(400), True)
 
     def test_should_return_false_if_divisible_by_100_but_not_by_400(self):
-        self.assertEqual(LeapYear.leapyear(356), False)
+        self.assertEqual(LeapYear.leapyear(500), False)
+
+    def test_should_return_true_if_divisible_by_4_but_not_by_100(self):
+        self.assertEqual(LeapYear.leapyear(104), True)
 
 
 if __name__ == '__main__':
