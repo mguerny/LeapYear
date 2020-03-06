917ab0cd1578911ec6d4bd76100b34cab98ad75a
mguerny <marin.guerny@laposte.net>
2020-03-06 12:19:43 +0100

    GREEN - test 4

--- leapyear.py
+++ leapyear.py
@@ -9,6 +9,9 @@ class LeapYear:
         if year % 100 == 0 and year % 400 != 0:
             return False
 
+        if year % 4 != 0:
+            return False
+
         if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
             return True
 
